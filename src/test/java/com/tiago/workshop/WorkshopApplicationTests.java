d6a326854f93133fa281da7aea3e5f86d1668ce2
Tiago Silva <tiagobsb84@gmail.com>
2022-05-15 15:37:43 -0300

    Projeto criado

---
+++ src/test/java/com/tiago/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
