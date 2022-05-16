b8277e05f53aeb96d11efee8472e4b45fe06dd67
Tiago Silva <tiagobsb84@gmail.com>
2022-05-15 23:47:28 -0300

    Obtendo um usuario pelo id

---
+++ src/main/java/com/tiago/workshop/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tiago.workshop.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String mensagem) {
+		super(mensagem);
+	}
+}
